91e157b92666272a404f68148198706524d5be2c
nicolaas <nicolaas@7b40dde9-fc51-43d4-b609-63a66f0a0cee>
2013-09-17 09:07:23 +0000

    UPGRADE 3.1
    git-svn-id: http://sunny.svnrepository.com/svn/sunny-side-up-general/prettyphoto/trunk@5297 7b40dde9-fc51-43d4-b609-63a66f0a0cee

--- code/PrettyPhoto.php
+++ code/PrettyPhoto.php
@@ -5,13 +5,10 @@ class PrettyPhoto extends Object {
 	private static $themes = array("dark_rounded", "dark_square", "facebook", "light_rounded", "light_square");
 
 	private static $theme = "";
-		static function set_theme($v) {if(!in_array($v, self::$themes)) {user_error("Please select a valid PrettyPhoto theme in PrettyPhoto::set_theme()", E_USER_WARNING);} self::$theme = $v;}
 
 	private static $more_config = "social_tools: false";
-		static function set_more_config($v) {self::$more_config = $v;}
 
 	private static $selector = "body";
-		static function set_selector($v) {self::$selector = $v;}
 
 	static function include_code() {
 		if(Director::is_ajax()) {
@@ -21,10 +18,6 @@ class PrettyPhoto extends Object {
 			Requirements::javascript(THIRDPARTY_DIR."/jquery/jquery.js");
 			Requirements::javascript('prettyphoto/javascript/jquery.prettyPhoto.js');
 			Requirements::css('prettyphoto/css/prettyPhoto.css');
-			// START DIRTY HACK!
-			Requirements::javascript('prettyPhoto/javascript/jquery.prettyPhoto.js');
-			Requirements::css('prettyPhoto/css/prettyPhoto.css');
-			// END DIRTY HACK
 
 			$config = '';
 			if(self::$theme) {
